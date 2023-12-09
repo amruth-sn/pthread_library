b0281478bd738d6fea7bb1abd7f142b92359b76a
amruth-sn <106125855+amruth-sn@users.noreply.github.com>
2023-12-09 01:23:56 -0500

    Create pthreader.h

---
+++ main/pthreader.h
@@ -0,0 +1,53 @@
+#ifndef __THREADS__
+#define __THREADS__
+
+unsigned long int ptr_demangle(unsigned long int p)
+{
+    unsigned long int ret;
+
+    asm("movq %1, %%rax;\n"
+        "rorq $0x11, %%rax;"
+        "xorq %%fs:0x30, %%rax;"
+        "movq %%rax, %0;"
+    : "=r"(ret)
+    : "r"(p)
+    : "%rax"
+    );
+    return ret;
+}
+
+unsigned long int ptr_mangle(unsigned long int p)
+{
+    unsigned long int ret;
+
+    asm("movq %1, %%rax;\n"
+        "xorq %%fs:0x30, %%rax;"
+        "rolq $0x11, %%rax;"
+        "movq %%rax, %0;"
+    : "=r"(ret)
+    : "r"(p)
+    : "%rax"
+    );
+    return ret;
+}
+
+void *start_thunk() {
+  asm("popq %%rbp;\n"           //clean up the function prolog
+      "movq %%r13, %%rdi;\n"    //put arg in $rdi
+      "pushq %%r12;\n"          //push &start_routine
+      "retq;\n"                 //return to &start_routine
+      :
+      :
+      : "%rdi"
+  );
+  __builtin_unreachable();
+}
+
+void pthread_exit_wrapper()
+{
+        unsigned long int res;
+        asm("movq %%rax, %0\n":"=r"(res));
+        pthread_exit((void *) res);
+}
+
+#endif
